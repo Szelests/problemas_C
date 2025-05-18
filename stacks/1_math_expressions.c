e9728cfeed0023032bd334686874a633efafd51b
Pedro Szelest <szelest@alunos.utfpr.edu.br>
2025-05-18 01:23:57 -0300

    Comenting about Shunting Yard algorithm

--- stacks/1_math_expressions.c
+++ stacks/1_math_expressions.c
@@ -1,3 +1,9 @@
+/*
+Escrever um programa em C para avaliar o resultado de expressões aritméticas posfixa utilizando as funções para manipulação de uma Pilha, com alocação dinâmica de memória. O usuário deverá entrar com uma expressão (string) formada por números inteiros e operadores aritméticos (+, -, *, /) separados por espaços em branco. A saída deve ser o resultado da expressão.
+
+---> Solução: usar o algorítmo de Shunting Yard, criado pelo Edsger Dijkstra (passa de infix para post-fixed)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
